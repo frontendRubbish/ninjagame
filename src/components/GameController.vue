fb99efa001d06d0cf1a649118873a03373a78dcd
prenger <prenger@sicksociety.net>
2019-10-13 14:33:19 +0200

    General setup with vue cli and gamepad detection

---
+++ src/components/GameController.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Controller</h1>
+</template>
+
+<script>
+
+let gamepad;
+
+function addConnectListener() {
+  window.addEventListener("gamepadconnected", connecthandler)
+}
+
+function connecthandler(e) {
+  addgamepad(e.gamepad)
+}
+
+function addgamepad( newGamepad ) {
+  gamepad = newGamepad;
+  console.log( gamepad )
+}
+
+export default {
+  name: 'GameController',
+  mounted: function () {
+    addConnectListener()
+  }
+}
+
+</script>
